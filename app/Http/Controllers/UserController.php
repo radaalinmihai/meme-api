683becb0a7fd894dfd10b0637cfda35dba0106dd
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-08-10 22:41:40 +0300

    User authentication works

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,24 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
 
-=======
->>>>>>> 9efe73f99b458bf9a6de8aeacf0faf5ffd18101e
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
 use Validator;
-<<<<<<< HEAD
 
 class UserController extends Controller
 {
     public $successStatus = 200;
-    /** 
-     * login api 
-     * 
-     * @return \Illuminate\Http\Response 
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
      */
     public function login(Request $request)
     {
@@ -30,34 +27,10 @@ class UserController extends Controller
             return response()->json(['error' => 'Unauthorised'], 401);
         }
     }
-    /** 
-     * Register api 
-     * 
-     * @return \Illuminate\Http\Response 
-=======
-class UserController extends Controller
-{
-public $successStatus = 200;
-/**
-     * login api
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function login(){
-        if(Auth::attempt(['username' => request('username'), 'password' => request('password')])){
-            $user = Auth::user();
-            $success['token'] =  $user->createToken('Meme')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
-        }
-        else{
-            return response()->json(['error'=>'Unauthorised'], 401);
-        }
-    }
-/**
+    /**
      * Register api
      *
      * @return \Illuminate\Http\Response
->>>>>>> 9efe73f99b458bf9a6de8aeacf0faf5ffd18101e
      */
     public function register(Request $request)
     {
@@ -67,56 +40,32 @@ public $successStatus = 200;
             'password' => 'required',
             'c_password' => 'required|same:password',
         ]);
-<<<<<<< HEAD
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 401);
         }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        $success['username'] =  $user->name;
+        $success['token'] =  $this->generateToken($user->email, $request->password);
+        $success['username'] =  $user->username;
         return response()->json(['success' => $success], $this->successStatus);
     }
-
     private function generateToken($username, $password)
     {
-        $http = new GuzzleHttp\Client;
-        $response = $http->post('http://192.168.0.154:8000/oauth/token', [
+        $http = new Client;
+        $response = $http->post(url('/oauth/token'), [
             'form_params' => [
                 'grant_type' => 'password',
-                'client_id' => env('OAUTH_CLIENT_ID'),
-                'client_secret' => env('OAUTH_CLIENT_SECRET'),
+                'client_id' => env('OAUTH_PASSWORD_CLIENT_ID'),
+                'client_secret' => env('OAUTH_PASSWORD_CLIENT_SECRET'),
                 'username' => $username,
                 'password' => $password,
                 'scope' => '*',
-            ],
+            ]
         ]);
-        return json_decode((string) $response->getBody(), true);
+        return json_decode((string) $response, true);
     }
-
-    /** 
-     * details api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */
-    public function details()
-    {
-        $user = Auth::user();
-        return response()->json(['success' => $user], $this->successStatus);
-    }
-=======
-if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
-        }
-$input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('Meme')-> accessToken;
-        $success['username'] =  $user->username;
-return response()->json(['success'=>$success], $this-> successStatus);
-    }
-/**
+    /**
      * details api
      *
      * @return \Illuminate\Http\Response
@@ -124,7 +73,6 @@ return response()->json(['success'=>$success], $this-> successStatus);
     public function details()
     {
         $user = Auth::user();
-        return response()->json(['success' => $user], $this-> successStatus);
+        return response()->json(['success' => $user], $this->successStatus);
     }
->>>>>>> 9efe73f99b458bf9a6de8aeacf0faf5ffd18101e
 }
