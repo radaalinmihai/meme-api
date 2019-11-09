8ea3272e05c805ce1a0409854cd67c0a71a57633
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-11-10 00:39:28 +0200

    Added error message on wrong credentials

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
             $success['token'] =  $this->generateToken($user->email, $request->password);
             return response()->json(['success' => $success], $this->successStatus);
         } else {
-            return response()->json(['error' => 'Unauthorised'], 401);
+            return response()->json(['error' => 'Username and password don\'t match.'], 401);
         }
     }
     /**
